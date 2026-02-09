d4058c75a14f63c533bd46f4677f843a487a12c9
weikang <2552820274@qq.com>
2026-02-09 21:38:24 +0800

    1.goctl、protoc、go-zero环境配置

---
+++ api/internal/handler/apihandler.go
@@ -0,0 +1,31 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package handler
+
+import (
+	"net/http"
+
+	"fengfengstudy/api/internal/logic"
+	"fengfengstudy/api/internal/svc"
+	"fengfengstudy/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func ApiHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewApiLogic(r.Context(), svcCtx)
+		resp, err := l.Api(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
